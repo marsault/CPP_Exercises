6e04704cf2f2f2a37d78241099e8a95b00000132
Victor Marsault <marsault@onodrim>
2025-02-24 08:53:51 +0100

    Simplified solution a bit

--- tp06/2-ptr/CopyablePtr.hpp
+++ tp06/2-ptr/CopyablePtr.hpp
@@ -7,52 +7,67 @@
 class CopyablePtr
 {
 public:
+    // We allow to create a null CopyablePtr;
     CopyablePtr() = default;
 
     CopyablePtr(int value)
-        : _obj { new Object { value } }
-    {}
+        : _obj{new Object{value}}
+    {
+    }
 
-    CopyablePtr(const CopyablePtr& other)
-        : _obj { other == nullptr ? nullptr : new Object { *other } }
-    {}
+    CopyablePtr(const CopyablePtr &other)
+        : _obj{other == nullptr ? nullptr : new Object{*other}}
+    {
+    }
 
-    CopyablePtr(CopyablePtr&& other) { std::swap(_obj, other._obj); }
+    CopyablePtr(CopyablePtr &&other)
+        : _obj{other._obj}
+    {
+        // We now manage the Object pointed by other
+        // We need to set the pointer of other to nullptr in order to avoir double-deallocation
+        other._obj = nullptr;
+    }
 
-    ~CopyablePtr() { delete _obj; }
+    ~CopyablePtr()
+    {
+        // We do not need to check if _obj == nullptr
+        // Deleting a nullptr does nothing
+        delete _obj;
+    }
 
     bool operator==(std::nullptr_t) const { return _obj == nullptr; }
 
-    Object& operator*() const { return *_obj; }
+    Object &operator*() const { return *_obj; }
 
-    CopyablePtr& operator=(std::nullptr_t)
+    // This operator allows to assign a nullptr to a CopyablePtr
+    CopyablePtr &operator=(std::nullptr_t)
     {
         delete _obj;
         _obj = nullptr;
-
         return *this;
     }
 
-    CopyablePtr& operator=(const CopyablePtr& other)
+    CopyablePtr &operator=(const CopyablePtr &other)
     {
         if (this != &other)
         {
             delete _obj;
-            _obj = other == nullptr ? nullptr : new Object { *other };
+            _obj = ((other == nullptr) ? nullptr : new Object{*other});
         }
         return *this;
     }
 
-    CopyablePtr& operator=(CopyablePtr&& other)
+    CopyablePtr &operator=(CopyablePtr &&other)
     {
         if (this != &other)
         {
-            *this = nullptr;
-            std::swap(_obj, other._obj);
+            delete _obj;
+            _obj = other._obj;
+            other._obj = nullptr;
         }
         return *this;
     }
 
 private:
-    Object* _obj = nullptr;
+    Object *_obj = nullptr;
 };
