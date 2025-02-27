6ba8fe8e2386caf343b826914415704978a25eda
Anthony Labarre <labarre.anthony@gmail.com>
2025-02-27 11:16:30 +0100

    tp07: ajout de Item.hpp

--- tp07/dungeon/Trap.hpp
+++ tp07/dungeon/Trap.hpp
@@ -1,6 +1,6 @@
 #include "Entity.hpp" // Entity
 
-// question C.1
+// question C.1 (version pré-refactorisation)
 class Trap : public Entity {
     public:
         Trap(int width, int height) : Entity(random_value(1, width-1), random_value(1, height-1)) {}
