26b6070c554858d266411fc2e7d2994b51d3df61
Anthony Labarre <labarre.anthony@gmail.com>
2025-04-04 10:14:52 +0200

    tp11: solution exo 1.1

--- tp11/src/factory.cpp
+++ tp11/src/factory.cpp
@@ -3,6 +3,8 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <functional> // std::function
+#include <map>
 
 // Liste des entitées à construire
 std::string desc = R"(Object
@@ -22,16 +24,41 @@ public:
 class Factory
 {
 public:
-    // using Builder = ...;
+    // exercice 1.1.1: comme Factory::build plus bas renvoie un std::unique_ptr<Entity>, on utilise
+    // également ce type de retour ici plutôt qu'un simple Entity
+    using Builder = std::function<std::unique_ptr<Entity>()>;
 
     template <typename TDerivedEntity>
-    void register_entity()
-    {}
+    // void register_entity(const std::string& id)
+    void register_entity(const std::string& id) /* 1.1.3: rajout de id */
+    {
+        // exercice 1.1.3: on rajoute à la map la clé id, et la valeur associée est une lambda qui
+        // invoquera le constructeur associé
+        _builders.emplace(
+            id, 
+            []() 
+            {
+                return std::make_unique<TDerivedEntity>();
+            }
+        );
+    }
 
-    std::unique_ptr<Entity> build(const std::string& id) const { return nullptr; }
+    std::unique_ptr<Entity> build(const std::string& id) const 
+    { 
+        // exercice 1.1.4: cherchons d'abord id pour éviter les plantages si _builders ne la 
+        // contient pas
+        auto it = _builders.find(id);
+
+        if(it != _builders.end())
+            return it->second();
+
+        return nullptr; 
+}
 
 private:
-    // ...
+    // exercice 1.1.2: les identifiants étant des chaînes, d'après la signature de Factory::build,
+    // il nous faudra une map pour réaliser l'association
+    std::map<std::string, Builder> _builders;
 };
 
 class Object : public Entity
@@ -91,7 +118,9 @@ private:
 int main()
 {
     Factory factory;
-    // factory.register_entity<Object>("Object");
+    // exercice 1.2: la sortie est bien Object, suivi de Tree
+    factory.register_entity<Object>("Object");
+    factory.register_entity<Tree>("Tree");
 
     std::vector<std::unique_ptr<Entity>> entities;
 
