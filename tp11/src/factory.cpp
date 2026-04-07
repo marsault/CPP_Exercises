4870200f4552ad23020cf826448c1a982683e75e
Victor Marsault <marsault@mercure>
2026-04-07 10:44:29 +0200

    Update of tp11

--- tp11/src/factory.cpp
+++ tp11/src/factory.cpp
@@ -10,108 +10,111 @@ Person
 Dog
 Cat
 Tree
-House
-)";
+House)";
 
 class Entity
 {
 public:
-    virtual void print() const = 0;
+  virtual void print() const = 0;
 };
 
 class Factory
 {
 public:
-    // using Builder = ...;
+  // using Builder =
 
-    template <typename TDerivedEntity>
-    void register_entity()
-    {}
+  template <typename TDerivedEntity>
+  void register_entity(std::string)
+  {}
 
-    std::unique_ptr<Entity> build(const std::string& id) const { return nullptr; }
+  std::unique_ptr<Entity> build(std::string) const { return nullptr; }
 
 private:
-    // ...
+  // ...
 };
 
 class Object : public Entity
 {
 public:
-    void print() const override { std::cout << "Object" << std::endl; }
+  void print() const override { std::cout << "Object" << std::endl; }
 };
 
 class Tree : public Object
 {
 public:
-    void print() const override { std::cout << "Tree" << std::endl; }
+  void print() const override { std::cout << "Arbre" << std::endl; }
 };
 
 class Person : public Entity
 {
 public:
-    explicit Person(std::string name)
-        : _name { std::move(name) }
-    {}
+  explicit Person(std::string name)
+      : _name{std::move(name)}
+  {}
 
-    void print() const override { std::cout << _name << std::endl; }
+  void print() const override { std::cout << "Person named " << _name << std::endl; }
 
-    void               set_name(std::string name) { _name = std::move(name); }
-    const std::string& get_name() const { return _name; }
+  void set_name(std::string name) { _name = std::move(name); }
+  const std::string& get_name() const { return _name; }
 
 private:
-    std::string _name;
+  std::string _name;
 };
 
 class Animal : public Entity
 {
 public:
-    explicit Animal(std::string species)
-        : _species { std::move(species) }
-    {}
+  explicit Animal(std::string species)
+      : _species{std::move(species)}
+  {}
 
-    void print() const override { std::cout << _species << std::endl; }
+  explicit Animal(std::string species, std::string name)
+      : _species{std::move(species)}
+      , _name{std::move(name)}
+  {}
 
-private:
-    std::string _species;
-};
-
-class House : public Object
-{
-public:
-    explicit House(Person& owner)
-        : _owner { owner }
-    {}
-
-    void print() const override { std::cout << "House owned by " << _owner.get_name() << std::endl; }
+  void print() const override
+  {
+    std::cout << "Animal of species " << _species;
+    if (_name != "")
+      std::cout << " named " << _name;
+    std::cout << std::endl;
+  }
 
 private:
-    Person& _owner;
+  std::string _species;
+  std::string _name;
 };
 
 int main()
 {
-    Factory factory;
-    // factory.register_entity<Object>("Object");
+  Factory factory;
+  // factory.register_entity<Object>("Object");
+  // ...
 
-    std::vector<std::unique_ptr<Entity>> entities;
 
-    std::stringstream s;
-    s << desc;
-    while (!s.eof())
-    {
-        std::string e;
-        s >> e;
+  // Vous n'avez rien à modifier en dessous de cette ligne !
+  std::vector<std::pair<std::string, std::unique_ptr<Entity>>> entities;
 
-        entities.emplace_back(factory.build(e));
-    }
+  std::stringstream s;
+  s << desc;
+  while (!s.eof()) {
+    std::string e;
+    s >> e;
 
-    for (const auto& e : entities)
-    {
-        if (e != nullptr)
-        {
-            e->print();
-        }
-    }
+    auto o = factory.build(e);
+    entities.emplace_back(std::move(e), std::move(o));
+  }
 
-    return 0;
+  for (const auto& e : entities) {
+    std::cout << "In:  " << e.first << std::endl;
+    std::cout << "Out: ";
+    if (e.second != nullptr)
+      e.second->print();
+    else
+      std::cout << "nullptr" << std::endl;
+    std::cout << std::endl;
+  }
+
+  return 0;
 }
