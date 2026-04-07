d138e4764fe9bdfc106487878b86c9b72716fefe
Victor Marsault <marsault@mercure>
2026-04-07 21:53:21 +0200

    Solution for tp11/tostring

--- tp11/src/tostring.cpp
+++ tp11/src/tostring.cpp
@@ -6,8 +6,20 @@
 #include <string>
 #include <vector>
 
+// Définition du type général
+template <unsigned N>
+struct priority_tag : priority_tag<N - 1>
+{};
+// Spécialisation de priority_tag pour N=0
+template <>
+struct priority_tag<0>
+{};
+
+priority_tag<10> priority_highest_value;
+
+
 template <typename T>
-auto to_string(T&& data)
+std::string to_string(T&& data, priority_tag<0>)
 {
   std::stringstream ss;
   ss << "<" << typeid(data).name() << ": " << &data << ">";
@@ -16,6 +28,110 @@ auto to_string(T&& data)
 
 // Ecrire les spécialisations ici
 // ...
+
+template <typename T>
+std::string to_string(T&& data);
+
+template <>
+std::string to_string(std::string& data)
+{
+  return data;
+}
+
+template <>
+std::string to_string(std::string&& data)
+{
+  return data;
+}
+
+
+// Ici, on shadow, on ne spécialise pas
+std::string to_string(char const* const& data)
+{
+  return data;
+}
+
+
+template <typename T>
+auto to_string(T&& data, priority_tag<6>) -> decltype(std::to_string(data))
+{
+  return std::to_string(data);
+}
+
+template <typename T>
+auto to_string(T&& data, priority_tag<5>) -> decltype(std::declval<std::ostream>() << data,
+                                                      std::string{})
+{
+  std::stringstream ss;
+  ss << data;
+  return ss.str();
+}
+
+template <typename T>
+auto to_string(T&& data, priority_tag<6>) -> decltype(data.to_string())
+{
+  return data.to_string();
+}
+
+template <typename T>
+auto to_string(T&& data, priority_tag<2>) -> decltype(data.begin(), std::string{})
+{
+  std::stringstream ss;
+  ss << "{";
+  bool first = true;
+  for (const auto& e : data) {
+    if (first)
+      first = false;
+    else
+      ss << ", ";
+    ss << to_string(e);
+  }
+  ss << "}";
+  return ss.str();
+}
+
+template <typename T, size_t i>
+std::string to_string(std::array<T, i>&& data)
+{
+  std::stringstream ss;
+  ss << "[";
+  bool first = true;
+  for (const auto& e : data) {
+    if (first)
+      first = false;
+    else
+      ss << ", ";
+    ss << to_string(e);
+  }
+  ss << "]";
+  return ss.str();
+}
+
+
+template <size_t i, typename... Args>
+void to_string_tuple(std::stringstream& ss, const std::string& s, const std::tuple<Args...>& tuple)
+{
+  if constexpr (i < std::tuple_size_v<std::tuple<Args...>>) {
+    ss << s << to_string(std::get<i>(tuple));
+    to_string_tuple<i + 1>(ss, ", ", tuple);
+  }
+}
+
+template <typename... Args>
+std::string to_string(std::tuple<Args...>&& tuple)
+{
+  std::stringstream ss;
+  to_string_tuple<0u, Args...>(ss, "(", tuple);
+  ss << ")";
+  return ss.str();
+}
+
+
+template <typename T>
+std::string to_string(T&& data)
+{
+  return to_string(std::forward<T>(data), priority_highest_value);
+}
 
 
 // Vous n'avez rien à modifier en dessous de cette ligne !
